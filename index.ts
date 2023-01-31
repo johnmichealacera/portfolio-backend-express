02deb21a92e134257c8d9a71e3198fd05753122b
John Acera <john.acera@sportality.se>
2023-01-31 20:51:39 +0800

    initial commit for portfolio backend in express

---
+++ index.ts
@@ -0,0 +1,23 @@
+var express = require('express');
+const mongoose = require("mongoose");
+const Project = require("./src/models/project.ts");
+var app = express();
+const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://jmann432015:L3E1IgbaXuyOUjI8@cluster0.o7ogr.mongodb.net/personal?authSource=admin&replicaSet=atlas-10vtn2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+    );
+    app.listen(5000, () => console.log("Server started on port 3000"));
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+app.use(express.json());
+
+start();
+
+app.get("/projects", async (req, res) => {
+  const allProjects = await Project.find({}, { __v: 0, _id: 0 }).lean();
+  return res.status(200).json(allProjects);
+});
---
+++ src/models/project.ts
@@ -0,0 +1,18 @@
+const mongooseInstance = require("mongoose");
+
+const ProjectSchema = new mongooseInstance.Schema({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  url: {
+    type: String,
+  },
+  imageLink: {
+    type: String,
+  },
+});
+
+module.exports = mongooseInstance.model("Project", ProjectSchema, "project");
